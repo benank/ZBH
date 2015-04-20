d8ee082ebf3e1108652cfd048444a9ec4c09a7f5
noobasaurus <rainbowdatabot@yahoo.com>
2015-04-19 21:38:36 -0700

    Initial Commit

---
+++ ZBHg_con.lua
@@ -0,0 +1,217 @@
+--
+-- Copyright (c) 2005 Pandemic Studios, LLC. All rights reserved.
+--
+
+ScriptCB_DoFile("setup_teams")
+ScriptCB_DoFile("ObjectiveConquest")
+
+	--  Empire Attacking (attacker is always #1)
+    local ALL = 2
+    local IMP = 1
+    --  These variables do not change
+    local ATT = 1
+    local DEF = 2
+    
+function ScriptPostLoad()	   
+    
+    --BEGINNING SETUP--
+	SetTeamPoints(1,0)
+	SetTeamPoints(2,0)
+	ShowTeamPoints(1,true)
+	ShowTeamPoints(2,true)
+	AddAIGoal(1, "deathmatch", 1000)
+	AddAIGoal(2, "deathmatch", 1000)
+	credits = 0
+	ScriptCB_SetCanSwitchSides(1)
+	Door1Unlocked = false
+	Door2Unlocked = false
+	Door3Unlocked = false
+	Door4Unlocked = false
+	Door5Unlocked = false
+	
+	SetUberMode(1);
+	--OnObjectKill(function(object,killer)
+	--	DeactivateObject(object)
+    --end)
+	
+ end
+
+
+---------------------------------------------------------------------------
+-- FUNCTION:    ScriptInit
+-- PURPOSE:     This function is only run once
+-- INPUT:
+-- OUTPUT:
+-- NOTES:       The name, 'ScriptInit' is a chosen convention, and each
+--              mission script must contain a version of this function, as
+--              it is called from C to start the mission.
+---------------------------------------------------------------------------
+
+function ScriptInit()
+    
+    ReadDataFile("ingame.lvl")
+
+
+
+    SetMaxFlyHeight(40000)
+	SetMaxPlayerFlyHeight(40000)
+
+
+	SetMemoryPoolSize ("ClothData",20)
+    SetMemoryPoolSize ("Combo",50)              -- should be ~ 2x number of jedi classes
+    SetMemoryPoolSize ("Combo::State",650)      -- should be ~12x #Combo
+    SetMemoryPoolSize ("Combo::Transition",650) -- should be a bit bigger than #Combo::State
+    SetMemoryPoolSize ("Combo::Condition",650)  -- should be a bit bigger than #Combo::State
+    SetMemoryPoolSize ("Combo::Attack",550)     -- should be ~8-12x #Combo
+    SetMemoryPoolSize ("Combo::DamageSample",6000)  -- should be ~8-12x #Combo::Attack
+    SetMemoryPoolSize ("Combo::Deflect",100)     -- should be ~1x #combo  
+    
+    
+    ReadDataFile("sound\\tat.lvl;tat2gcw")
+    ReadDataFile("dc:sound\\tss.lvl;tssgcw")
+    ReadDataFile("SIDE\\all.lvl",
+                    "all_inf_rifleman",
+                    "all_inf_rocketeer",
+                    "all_inf_sniper",
+                    "all_inf_engineer",
+                    "all_inf_officer",
+                    "all_inf_wookiee",
+                    "all_hero_hansolo_tat")
+                    
+    ReadDataFile("SIDE\\imp.lvl",
+                    "imp_inf_rifleman",
+                    "imp_inf_rocketeer",
+                    "imp_inf_engineer",
+                    "imp_inf_sniper",
+                    "imp_inf_officer",
+                    "imp_inf_dark_trooper",
+                    "imp_hero_bobafett",
+                    "imp_fly_destroyer_dome" )
+
+	ReadDataFile("dc:SIDE\\zbh.lvl",
+						"zbh_inf_player1",
+						"zbh_inf_tank",
+						"zbh_inf_zombie",
+						"zbh_inf_jumper",
+						"zbh_inf_crawler")
+ 
+	SetupTeams{
+		all = {
+			team = ALL,
+			units = 20,
+			reinforcements = 1,
+			soldier	= { "zbh_inf_player1",0, 20},
+			assault	= { "all_inf_rocketeer",0,0},
+			engineer = { "all_inf_engineer",0,0},
+			sniper	= { "all_inf_sniper",0,0},
+			officer	= { "all_inf_officer",0,0},
+			special	= { "all_inf_wookiee",0,0},
+
+		},
+		imp = {
+			team = IMP,
+			units = 200,
+			reinforcements = -1,
+			soldier	= { "zbh_inf_crawler",0, 0},
+			assault	= { "zbh_inf_tank",0,2},
+			engineer = { "zbh_inf_jumper",0,0},
+			sniper	= { "zbh_inf_zombie",0,0},
+			officer	= { "imp_inf_officer",0,0},
+			special	= { "imp_inf_dark_trooper",0,0},
+		},
+	}
+    
+    --  Level Stats
+    ClearWalkers()
+    AddWalkerType(0, 0) -- special -> droidekas
+    AddWalkerType(1, 0) -- 1x2 (1 pair of legs)
+    AddWalkerType(2, 0) -- 2x2 (2 pairs of legs)
+    AddWalkerType(3, 0) -- 3x2 (3 pairs of legs)
+    
+    local weaponCnt = 1024
+    SetMemoryPoolSize("Aimer", 75)
+    SetMemoryPoolSize("AmmoCounter", weaponCnt)
+    SetMemoryPoolSize("BaseHint", 1024)
+    SetMemoryPoolSize("EnergyBar", weaponCnt)
+	SetMemoryPoolSize("EntityCloth", 32)
+	SetMemoryPoolSize("EntityFlyer", 32)
+    SetMemoryPoolSize("EntityHover", 32)
+    SetMemoryPoolSize("EntityLight", 200)
+    SetMemoryPoolSize("EntitySoundStream", 4)
+    SetMemoryPoolSize("EntitySoundStatic", 32)
+    SetMemoryPoolSize("MountedTurret", 32)
+	SetMemoryPoolSize("Navigator", 128)
+    SetMemoryPoolSize("Obstacle", 1024)
+    SetMemoryPoolSize("ParticleTransformer::SizeTransf", 1150)
+	SetMemoryPoolSize("PathNode", 1024)
+    SetMemoryPoolSize("SoldierAnimation", 640)
+    SetMemoryPoolSize("SoundSpaceRegion", 64)
+    SetMemoryPoolSize("TreeGridStack", 1024)
+	SetMemoryPoolSize("UnitAgent", 128)
+	SetMemoryPoolSize("UnitController", 128)
+	SetMemoryPoolSize("Weapon", weaponCnt)
+
+    SetSpawnDelay(10.0, 0.25)
+    ReadDataFile("dc:ZBH\\ZBH.lvl", "ZBH_conquest")
+    SetDenseEnvironment("false")
+
+
+    --  Sound Stats
+    
+    voiceSlow = OpenAudioStream("sound\\global.lvl", "all_unit_vo_slow")
+    AudioStreamAppendSegments("sound\\global.lvl", "imp_unit_vo_slow", voiceSlow)
+    AudioStreamAppendSegments("sound\\global.lvl", "des_unit_vo_slow", voiceSlow)
+    AudioStreamAppendSegments("sound\\global.lvl", "global_vo_slow", voiceSlow)
+    
+    voiceQuick = OpenAudioStream("sound\\global.lvl",  "all_unit_vo_quick")
+    AudioStreamAppendSegments("sound\\global.lvl",  "imp_unit_vo_quick", voiceQuick)    
+    
+    OpenAudioStream("sound\\global.lvl",  "gcw_music")
+    OpenAudioStream("sound\\tat.lvl",  "tat2")
+    OpenAudioStream("sound\\tat.lvl",  "tat2")
+    -- OpenAudioStream("sound\\global.lvl",  "global_vo_quick")
+    -- OpenAudioStream("sound\\global.lvl",  "global_vo_slow")
+
+    SetBleedingVoiceOver(ALL, ALL, "all_off_com_report_us_overwhelmed", 1)
+    SetBleedingVoiceOver(ALL, IMP, "all_off_com_report_enemy_losing",   1)
+    SetBleedingVoiceOver(IMP, ALL, "imp_off_com_report_enemy_losing",   1)
+    SetBleedingVoiceOver(IMP, IMP, "imp_off_com_report_us_overwhelmed", 1)
+
+    SetLowReinforcementsVoiceOver(ALL, ALL, "all_off_defeat_im", .1, 1)
+    SetLowReinforcementsVoiceOver(ALL, IMP, "all_off_victory_im", .1, 1)
+    SetLowReinforcementsVoiceOver(IMP, IMP, "imp_off_defeat_im", .1, 1)
+    SetLowReinforcementsVoiceOver(IMP, ALL, "imp_off_victory_im", .1, 1)
+
+    SetOutOfBoundsVoiceOver(2, "Allleaving")
+    SetOutOfBoundsVoiceOver(1, "Impleaving")
+
+    SetAmbientMusic(ALL, 1.0, "all_tat_amb_start",  0,1)
+    SetAmbientMusic(ALL, 0.8, "all_tat_amb_middle", 1,1)
+    SetAmbientMusic(ALL, 0.2, "all_tat_amb_end",    2,1)
+    SetAmbientMusic(IMP, 1.0, "imp_tat_amb_start",  0,1)
+    SetAmbientMusic(IMP, 0.8, "imp_tat_amb_middle", 1,1)
+    SetAmbientMusic(IMP, 0.2, "imp_tat_amb_end",    2,1)
+
+    SetVictoryMusic(ALL, "all_tat_amb_victory")
+    SetDefeatMusic (ALL, "all_tat_amb_defeat")
+    SetVictoryMusic(IMP, "imp_tat_amb_victory")
+    SetDefeatMusic (IMP, "imp_tat_amb_defeat")
+
+    SetSoundEffect("ScopeDisplayZoomIn",  "binocularzoomin")
+    SetSoundEffect("ScopeDisplayZoomOut", "binocularzoomout")
+    --SetSoundEffect("WeaponUnableSelect",  "com_weap_inf_weaponchange_null")
+    --SetSoundEffect("WeaponModeUnableSelect",  "com_weap_inf_modechange_null")
+    SetSoundEffect("SpawnDisplayUnitChange",       "shell_select_unit")
+    SetSoundEffect("SpawnDisplayUnitAccept",       "shell_menu_enter")
+    SetSoundEffect("SpawnDisplaySpawnPointChange", "shell_select_change")
+    SetSoundEffect("SpawnDisplaySpawnPointAccept", "shell_menu_enter")
+    SetSoundEffect("SpawnDisplayBack",             "shell_menu_exit")
+
+    --  Camera Stats
+    --Tat2 Mos Eisley
+	AddCameraShot(0.974338, -0.222180, 0.035172, 0.008020, -82.664650, 23.668301, 43.955681);
+	AddCameraShot(0.390197, -0.089729, -0.893040, -0.205362, 23.563562, 12.914885, -101.465561);
+	AddCameraShot(0.169759, 0.002225, -0.985398, 0.012916, 126.972809, 4.039628, -22.020613);
+	AddCameraShot(0.677453, -0.041535, 0.733016, 0.044942, 97.517807, 4.039628, 36.853477);
+	AddCameraShot(0.866029, -0.156506, 0.467299, 0.084449, 7.685640, 7.130688, -10.895234);
+end
